a1b82a6fea5339f229bca506ab0a3c4c1481fc35
Kuznetsov Nikita Igorevich <n.kuznetsov@tinkoff.ru>
2019-06-17 22:48:24 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by n.kuznetsov on 17/06/2019.
+//  Copyright © 2019 n.kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
