6f89ecb6f62af4eab85c9aa91886ca115fb47abb
Kuznetsov Nikita Igorevich <n.kuznetsov@tinkoff.ru>
2019-07-12 18:21:30 +0300

    add views with constaints

---
+++ Notes/ColorSquareView.swift
@@ -0,0 +1,13 @@
+//
+//  ColorSquareView.swift
+//  Notes
+//
+//  Created by n.kuznetsov on 12/07/2019.
+//  Copyright © 2019 n.kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+
+class ColorSquareView: UIView {
+}
--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+//    private var whiteSquare: ColorSquareView!
+//    private var greenSquare: ColorSquareView!
+//    private var redSquare: ColorSquareView!
+//    private var anySquare: ColorSquareView!
+    @IBOutlet weak var whiteSquare: ColorSquareView!
+    @IBOutlet weak var redSquare: ColorSquareView!
+    @IBOutlet weak var greenSquare: ColorSquareView!
+    @IBOutlet weak var textField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        whiteSquare.layer.borderWidth = 1
+        whiteSquare.layer.borderColor = UIColor.black.cgColor
+        
+        redSquare.layer.borderWidth = 1
+        redSquare.layer.borderColor = UIColor.black.cgColor
+        
+        greenSquare.layer.borderWidth = 1
+        greenSquare.layer.borderColor = UIColor.black.cgColor
+//
+//        anySquare.layer.borderWidth = 1
+//        anySquare.layer.borderColor = UIColor.black.cgColor
     }
 
 
