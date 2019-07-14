2dd62b765108a2c4c14835b09afe50cc787aacc7
Kuznetsov Nikita Igorevich <n.kuznetsov@tinkoff.ru>
2019-07-14 19:30:38 +0300

    add xib for datepicker

---
+++ Notes/DatePickerView.swift
@@ -0,0 +1,36 @@
+//
+//  datePickerView.swift
+//  Notes
+//
+//  Created by n.kuznetsov on 14/07/2019.
+//  Copyright © 2019 n.kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DatePickerView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupViews()
+    }
+    
+    func setupViews() {
+        let xib = loadViewFromXib()
+        xib.frame = self.bounds
+        xib.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.addSubview(xib)
+    }
+    
+    private func loadViewFromXib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "DatePickerView", bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first! as! UIView
+    }
+}
